79fb1b594f33a7173597258d4ea78c29edf9de3a
Nate Eckerson <nate.eckerson@gmail.com>
2018-06-13 13:54:11 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,6 +12,6 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board_array, board_index, character = x)
+def move(board_array, board_index, character = X)
   board_array[board_index] = character
 end
